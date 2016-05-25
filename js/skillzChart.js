428bedb49a3a3a14c4a3f8a92b08f44ce81c1477
Nate <npauzenga@gmail.com>
2016-05-25 10:17:37 -0400

    layout and design updates

--- js/skillzChart.js
+++ js/skillzChart.js
@@ -1,11 +1,8 @@
 var rubyCtx = document.getElementById("ruby");
 var jsCtx = document.getElementById("javascript");
-var jqCtx = document.getElementById("jquery");
-var tddCtx = document.getElementById("tdd");
 var angularCtx = document.getElementById("angular");
 var htmlCtx = document.getElementById("html");
 var wpCtx = document.getElementById("wordpress");
-var bsCtx = document.getElementById("bootstrap");
 
 var ruby = new Chart(rubyCtx, {
  type: 'doughnut',
@@ -16,7 +13,7 @@ var ruby = new Chart(rubyCtx, {
    ],
    datasets: [
      {
-       data: [300, 500],
+       data: [600, 200],
        backgroundColor: [
          "#FF6A5C",
          "#056571"
@@ -34,7 +31,7 @@ var javascript = new Chart(jsCtx, {
    ],
    datasets: [
      {
-       data: [300, 500],
+       data: [500, 300],
        backgroundColor: [
          "#FF6A5C",
          "#056571"
@@ -52,7 +49,7 @@ var angular = new Chart(angularCtx, {
    ],
    datasets: [
      {
-       data: [300, 500],
+       data: [500, 300],
        backgroundColor: [
          "#FF6A5C",
          "#056571"
@@ -70,7 +67,7 @@ var html = new Chart(htmlCtx, {
    ],
    datasets: [
      {
-       data: [300, 500],
+       data: [600, 200],
        backgroundColor: [
          "#FF6A5C",
          "#056571"
@@ -80,60 +77,6 @@ var html = new Chart(htmlCtx, {
 });
 
 var wordpress = new Chart(wpCtx, {
- type: 'doughnut',
- data: {
-   labels: [
-     "watermelon",
-     "teal"
-   ],
-   datasets: [
-     {
-       data: [300, 500],
-       backgroundColor: [
-         "#FF6A5C",
-         "#056571"
-       ]
-     }]
- }
-});
-
-var bootstrap = new Chart(bsCtx, {
- type: 'doughnut',
- data: {
-   labels: [
-     "watermelon",
-     "teal"
-   ],
-   datasets: [
-     {
-       data: [300, 500],
-       backgroundColor: [
-         "#FF6A5C",
-         "#056571"
-       ]
-     }]
- }
-});
-
-var jquery = new Chart(jqCtx, {
- type: 'doughnut',
- data: {
-   labels: [
-     "watermelon",
-     "teal"
-   ],
-   datasets: [
-     {
-       data: [300, 500],
-       backgroundColor: [
-         "#FF6A5C",
-         "#056571"
-       ]
-     }]
- }
-});
-
-var tdd = new Chart(tddCtx, {
  type: 'doughnut',
  data: {
    labels: [
